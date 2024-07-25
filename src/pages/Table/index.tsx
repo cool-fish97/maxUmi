3db8c56f2770e1aac69a1fc6a72fbdea49e2e120
lyhao <coolyee07@163.com>
2024-07-25 14:06:30 +0800

    feat：修改误删属性报错

--- src/pages/Table/index.tsx
+++ src/pages/Table/index.tsx
@@ -94,7 +94,7 @@ const TableList: React.FC<unknown> = () => {
     {
       title: '名称',
       dataIndex: 'name',
-      tip: '名称是唯一的 key',
+      tooltip: '名称是唯一的 key',
       formItemProps: {
         rules: [
           {
